29c9bd03edcfdd3db472a5a1640349e329d6bfed
somang <somang.nam@gmail.com>
2019-04-17 23:43:52 -0400

    graph almost done, except ratings

--- sdt.py
+++ sdt.py
@@ -8,105 +8,18 @@ from sklearn import metrics
 from sklearn.metrics import roc_auc_score
 
 import matplotlib.pyplot as plt
+from scipy import stats
 from scipy.stats import norm
-from math import exp,sqrt
 
+from math import exp,sqrt,pi
 
-
-Z = norm.ppf
-#fig, ax = plt.subplots(1, 1)
-
-
-#The probability density function for norm is:
-# norm.pdf(x) = exp(-x**2/2)/sqrt(2*pi) for a real number x.
-# The probability density above is defined in the “standardized” form. 
-# To shift and/or scale the distribution use the loc and scale parameters. 
-# Specifically, norm.pdf(x, loc, scale) is identically equivalent to
-# norm.pdf(y) / scale with y = (x - loc) / scale.
-
-
-# if you accidentally write scipy.stats.norm(mean=100, std=12) instead of
-#  scipy.stats.norm(100, 12) or scipy.stats.norm(loc=100, scale=12), then it'll accept it,
-#  but silently discard those extra keyword arguments and give you the default (0,1)
-
-# print(norm.cdf(x, mean, sd))
-
-"""
-Here is more info. 
-First you are dealing with a frozen distribution
- (frozen in this case means its parameters are set to specific values). 
- 
- To create a frozen distribution:
-
-import scipy.stats
-scipy.stats.norm(loc=100, scale=12)
-#where loc is the mean and scale is the std dev
-#if you wish to pull out a random number from your distribution
-scipy.stats.norm.rvs(loc=100, scale=12)
-
-#To find the probability that the variable has a value LESS than or equal
-#let's say 113, you'd use CDF cumulative Density Function
-scipy.stats.norm.cdf(113,100,12)
-Output: 0.86066975255037792
-#or 86.07% probability
-
-#To find the probability that the variable has a value GREATER than or
-#equal to let's say 125, you'd use SF Survival Function 
-scipy.stats.norm.sf(125,100,12)
-Output: 0.018610425189886332
-# or 1.86%
-
-#To find the variate for which the probability is given, let's say the 
-#value which needed to provide a 98% probability, you'd use the 
-#PPF Percent Point Function
-scipy.stats.norm.ppf(.98,100,12)
-Output: 124.64498692758187
-
-
-print(norm.sf(0, 0, 1))
-print(norm.sf(0, 1.075, 1))
-
-
-# Display the probability density function (pdf)
-x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
-ax.plot(x, norm.pdf(x),'r-', lw=5, alpha=0.6, label='norm pdf')
-
-# the distribution object can be called (as a function) to fix the shape, location and scale parameters. 
-# This returns a “frozen” RV object holding the given parameters fixed.
-#Freeze the distribution and display the frozen pdf:
-rv = norm()
-ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
-
-# Check accuracy of cdf and ppf:
-vals = norm.ppf([0.001, 0.5, 0.999])
-print(np.allclose([0.001, 0.5, 0.999], norm.cdf(vals)))
-
-# Generate random numbers:
-r = norm.rvs(size=1000)
-# And compare the histogram:
-ax.hist(r, normed=True, histtype='stepfilled', alpha=0.2)
-ax.legend(loc='best', frameon=False)
-plt.show()
-
-
-matrix convolution
-
-"""
 
 
 """
 https://towardsdatascience.com/receiver-operating-characteristic-curves-demystified-in-python-bd531a4364d0
 """
-def pdf(x, sd, mean):
-    const = 1.0 / np.sqrt(2*np.pi*(sd**2))
-    pdf_normal_dist = const*np.exp(-((x-mean)**2)/(2.0*(sd**2)))
-    return pdf_normal_dist
 
-def plot_pdf(ideal_pdf, error_pdf, ax):
-    ax.fill(x, ideal_pdf, "g", alpha=0.5)
-    ax.fill(x, error_pdf,"r", alpha=0.5)
-    ax.set_xlim([0,1])
-    ax.set_ylim([0,5])
+def plot_pdf(ideal_pdf, error_pdf, xrange, ax):
     ax.set_title("Probability Distribution", fontsize=14)
     ax.set_ylabel('Probability Density', fontsize=12)
     ax.set_xlabel('Z-score', fontsize=12)
@@ -206,19 +119,7 @@ if __name__ == "__main__":
     sdt_obj = SDT(HI=18, MI=12, CR=3, FA=27)
     #print(sdt_obj)
     print(sdt_obj.c())
-    print(sdt_obj.dprime())
-    """
-
-    """
-    y_actual = np.array([1, 1, 2, 2])
-    y_scores = np.array([0.1, 0.4, 0.35, 0.8])
-    fpr, tpr, thresholds = metrics.roc_curve(y_actual, y_scores, pos_label=2)
-    roc_auc = roc_auc_score(y_actual, y_scores)
-    print(fpr)
-    print(tpr)
-    print(thresholds)
-    print(roc_auc)
-    """
+    print(sdt_obj.dprime())    
 
     ########################################################################################################################
     files = ['a_rating_data.in', 'd_rating_data.in', 'h_rating_data.in']
@@ -236,29 +137,56 @@ if __name__ == "__main__":
                 fpr = list(map(lambda x: float(x), fpr))
                 fname = input_file.split('_')[0] + "_" + hit_list[0].replace(":", "_") # a_V1_No Delay error (3 sec.)        
                 plot_roc_curve(fpr, tpr, fname, True)
-    
-    
-                
-    
     """
-    
-    tpr = [0.299, 0.598, 0.790, 0.931, 0.990, 1] # hit rate: y-axis of ROC
-    fpr = [0.019, 0.120, 0.253, 0.565, 0.834, 1] # false-alarm rate: x-axis of ROC
-    plot_roc_curve(fpr, tpr, 'testing.', True)  
+
+
+    # What do I need?
+    # input: factor variable value x
+    # output: probability of hit (H) and false-alarm (FA)
     
 
+    # v15_overall
+    dprime = 0.330
+    c = 0.111
+    # v2_overall
+    #dprime = -0.299
+    #c = 0.426
     
-    ######################################################
-    x = np.linspace(0, 1, num=100)
-    good_pdf = pdf(x, 0.1, 0.4)
-    bad_pdf = pdf(x, 0.1, 0.6)
 
-    fig, ax = plt.subplots(1,2, figsize=(10,5))
-    plot_pdf(good_pdf, bad_pdf, ax[0])
-    plot_roc(good_pdf, bad_pdf, ax[1])
+    # Display the probability density function (pdf)
+    x = np.linspace(-5, 5, 100)     # define a big enough x interval 
+    fig, ax = plt.subplots(1, 1, figsize=(7,5))
+
+    noise_pdf = norm.pdf(x)
+    error_pdf = norm.pdf(x, dprime, 1)              # get the norm.pdf for x interval
+    plt.plot(x, noise_pdf, "g", alpha=0.5, label="noise") #fill
+    plt.plot(x, error_pdf, "r", alpha=0.5, label="error")
+    plt.axvline(x=c, color='m', linestyle='-.') #, label="c= "+str(c))
+    plt.text(c+0.01, 0.01, "c= "+str(c))
+
+    plt.vlines(0, 0, 0.4, color='lightblue', linestyle=':')
+    plt.vlines(dprime, 0, 0.4, color='c', linestyle=':')
+    plt.hlines(0.4, 0, dprime) #, label="d'= "+str(dprime))
+    plt.text(dprime, 0.405, "d'= "+str(dprime), fontsize=10)
+    
+    ax.fill_between(x, noise_pdf, error_pdf, where=x > c, facecolor='green', alpha=0.1, interpolate=True)
+    ax.fill_between(x, noise_pdf, 0, where=x > c, facecolor='red', alpha=0.1, interpolate=True)
+
+
+    ax.set_xlim([-5,5])
+    ax.set_ylim([0, 0.5])
+    ax.set_title("Probability Distribution", fontsize=14)
+    ax.set_ylabel('Probability Density', fontsize=12)
+    ax.set_xlabel('Z-score', fontsize=12)
+    ax.legend()
+
     plt.tight_layout()
     plt.show()
 
+
+
+    """
+    
     ######################################################
     x = np.linspace(0, 1, num=100)
     fig, ax = plt.subplots(3,2, figsize=(10,12))
@@ -271,4 +199,73 @@ if __name__ == "__main__":
         plot_roc(good_pdf, bad_pdf, ax[i,1])
         i+=1
     plt.tight_layout()
+    
+
+    
+    
+    
+     if you accidentally write scipy.stats.norm(mean=100, std=12) instead of
+     scipy.stats.norm(100, 12) or scipy.stats.norm(loc=100, scale=12), then it'll accept it,
+     but silently discard those extra keyword arguments and give you the default (0,1)
+
+    print(norm.cdf(x, mean, sd))
+
+    Here is more info. 
+    First you are dealing with a frozen distribution
+    (frozen in this case means its parameters are set to specific values). 
+    
+    To create a frozen distribution:
+    import scipy.stats
+    scipy.stats.norm(loc=100, scale=12)
+    #where loc is the mean and scale is the std dev
+    #if you wish to pull out a random number from your distribution
+    scipy.stats.norm.rvs(loc=100, scale=12)
+
+    #To find the probability that the variable has a value LESS than or equal
+    #let's say 113, you'd use CDF cumulative Density Function
+    scipy.stats.norm.cdf(113,100,12)
+    Output: 0.86066975255037792
+    #or 86.07% probability
+
+    #To find the probability that the variable has a value GREATER than or
+    #equal to let's say 125, you'd use SF Survival Function 
+    scipy.stats.norm.sf(125,100,12)
+    Output: 0.018610425189886332
+    # or 1.86%
+
+    #To find the variate for which the probability is given, let's say the 
+    #value which needed to provide a 98% probability, you'd use the 
+    #PPF Percent Point Function
+    scipy.stats.norm.ppf(.98,100,12)
+    Output: 124.64498692758187
+
+
+    print(norm.sf(0, 0, 1))
+    print(norm.sf(0, 1.075, 1))
+
+
+    # Display the probability density function (pdf)
+    x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
+    ax.plot(x, norm.pdf(x),'r-', lw=5, alpha=0.6, label='norm pdf')
+
+    # the distribution object can be called (as a function) to fix the shape, location and scale parameters. 
+    # This returns a “frozen” RV object holding the given parameters fixed.
+    #Freeze the distribution and display the frozen pdf:
+    rv = norm()
+    ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+
+    # Check accuracy of cdf and ppf:
+    vals = norm.ppf([0.001, 0.5, 0.999])
+    print(np.allclose([0.001, 0.5, 0.999], norm.cdf(vals)))
+
+    # Generate random numbers:
+    r = norm.rvs(size=1000)
+    # And compare the histogram:
+    ax.hist(r, normed=True, histtype='stepfilled', alpha=0.2)
+    ax.legend(loc='best', frameon=False)
+    plt.show()
+
+
+    matrix convolution
+
     """
