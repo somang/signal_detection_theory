fb39ff0e20a9a4839ca4282c9c1a9bd809baff2b
somang <somang.nam@gmail.com>
2019-05-03 07:04:32 -0400

    update

--- csv_handler.py
+++ csv_handler.py
@@ -31,6 +31,8 @@ def to_score(answer):
     # '5. Strongly agree': 100%
     # weights are multiplied to the score.
     # 
+
+
     if answer == "1. Strongly dissatisfied":
         return 1
     elif answer == "2. Dissatisfied":
@@ -43,20 +45,20 @@ def to_score(answer):
         return 5
     
     if answer == "1. Strongly disagree":
-        return 0.2
-    elif answer == "2. Disagree":
-        return 0.4
-    elif answer == "3. Neither agree nor disagree":
-        return 0.6
-    elif answer == "4. Agree":
-        return 0.8
-    elif answer == "5. Strongly agree":
         return 1
+    elif answer == "2. Disagree":
+        return 2
+    elif answer == "3. Neither agree nor disagree":
+        return 3
+    elif answer == "4. Agree":
+        return 4
+    elif answer == "5. Strongly agree":
+        return 5
 
     if answer == "1. Yes":
         return 1
     elif answer == "2. No":
-        return -1
+        return 2
 
     
 def get_weight_score(x):
@@ -153,18 +155,19 @@ for sn in xl.sheet_names:
             if not nar[quiz].get(caption_var):
                 nar[quiz][caption_var] = {}
             if not nar[quiz][caption_var].get(uuid):
-                nar[quiz][caption_var][uuid] = answer            
+                nar[quiz][caption_var][uuid] = (video, answer)
+
         elif video in no_narrator_vid:
             if not no_nar[quiz].get(caption_var):
                 no_nar[quiz][caption_var] = {}
             if not no_nar[quiz][caption_var].get(uuid):
-                no_nar[quiz][caption_var][uuid] = answer
+                no_nar[quiz][caption_var][uuid] = (video, answer)
 
         # lets group by variations.
         if not variations[quiz].get(caption_var):
             variations[quiz][caption_var] = {}
         if not variations[quiz][caption_var].get(uuid):
-            variations[quiz][caption_var][uuid] = answer
+            variations[quiz][caption_var][uuid] = (video, answer)
 
 
     ## lets calculate t-tests over the columns we want,
@@ -177,19 +180,73 @@ for sn in xl.sheet_names:
     #     print(str(s[1][1]))
 
     # now we have a sorted table.
-    all_list, uids = [], []
+    all_list_video, all_list, nar_list, no_nar_list, uids = [], [], [], [], []
     for uid in variations["Did you see any errors in the caption?"]['1']: # get all uids
         uids.append(uid)
-
     
-    for q in variations:
-        for u in uids:
-            for n in range(1,24): # caption variations
-                var_n = str(n)
-                if nar[q][var_n].get(u):
-                    print("nar", u, nar[q][var_n][u])
-                elif no_nar[q][var_n].get(u):
-                    print("no nar", u, no_nar[q][var_n][u])
+    for q in variations: # for each question
+        qz, qz_video, nar_q, no_nar_q = [], [], [], []
+        head_row = ["uuid"]
+        first = True
+        for u in uids: # for each user
+            row, vid_row, nar_r, no_nar_r = [u], [u], [u], [u]
+            
+            for n in range(1, 24): # from variation 1 to 23            
+                variation_n = str(n)
+                if first:
+                    head_row.append(variation_n)
+                ans = (100, "")
+                
+                # if n < 23: # because v23 was not added for some user
+                #     ans = variations[q][variation_n][u] # add the answer of this user at this variation.
+                    
+                    # if nar[q][variation_n].get(u):
+                    #     ans = nar[q][variation_n][u]
+                    #     nar_q.append(ans[1])
+                    #     print("nar", u, variation_n, nar[q][variation_n][u])
+
+                    # elif no_nar[q][variation_n].get(u):
+                    #     ans = no_nar[q][variation_n][u]
+                    #     no_nar_q.append(ans[0])
+                    #     print("no nar", u, variation_n, no_nar[q][variation_n][u])
+
+                #elif n == 23:
+                if variations[q][variation_n].get(u):
+                    ans = variations[q][variation_n][u]
+
+                row.append(to_score(ans[1]))
+                vid_row.append(ans[0])
+            if first:
+                qz.append(head_row)
+                qz_video.append(head_row)
+                nar_q.append(head_row)
+                no_nar_q.append(head_row)
+                first = False            
+            qz.append(row) # for each row, it will have a username, along with the answers for each variation number 1-23
+            qz_video.append(vid_row)
+            nar_q.append(nar_r)
+            no_nar_q.append(no_nar_r)
+
+        all_list.append(qz)# add the row to the queue
+        all_list_video.append(qz_video)
+        nar_list.append(nar_q)
+        no_nar_list.append(no_nar_q)
+
+
+    worksheet = workbook.add_worksheet('q1_' + sn)
+    export_excel(worksheet, all_list[0])
+    worksheet = workbook.add_worksheet('q1_v_' + sn)
+    export_excel(worksheet, all_list_video[0])
+
+    worksheet = workbook.add_worksheet('q2_' + sn)
+    export_excel(worksheet, all_list[1])
+    worksheet = workbook.add_worksheet('q2_v_' + sn)
+    export_excel(worksheet, all_list_video[1])
+
+    worksheet = workbook.add_worksheet('q3_' + sn)
+    export_excel(worksheet, all_list[2])
+    worksheet = workbook.add_worksheet('q3_v_' + sn)
+    export_excel(worksheet, all_list_video[2])
 
 
 
@@ -206,48 +263,16 @@ for sn in xl.sheet_names:
 
 
 
+# #     # now that we have the raw values, let's calculate the weighted scores.
+# #     q4 = []
+# #     for r1 in all_list[0]:
+# #         for r2 in all_list[1]:
+# #             if (r1[0] == r2[0]) and (r1[0] != "uuid"):
+# #                 new_val_row = r1[:1] + list(map(lambda x: get_weight_score(x), zip(r1[1:], r2[1:])))
+# #                 q4.append(new_val_row)
+# #     all_list.append(q4)            
 
-#     for q in variations: # for each question
-#         qz = []
-#         head_row = ["uuid"]
-#         first = True
-#         for u in uids: # for each user
-#             row = [u]
-#             for n in range(1, 24): # from variation 1 to 23            
-#                 var_n = str(n)
-#                 if first:
-#                     head_row.append(var_n)
-#                 ans = 100
-#                 if n < 23: # because v23 was not added for some user
-#                     ans = variations[q][var_n][u] # add the answer of this user at this variation.
-#                 elif n == 23:
-#                     if variations[q][var_n].get(u):
-#                         ans = variations[q][var_n][u]
-#                 ans = to_score(ans)
-#                 row.append(ans)
-#             if first:
-#                 qz.append(head_row)
-#                 first = False
-#             qz.append(row) # for each row, it will have a username, along with the answers for each variation number 1-23
-#         all_list.append(qz)# add the row to the queue
+#     # worksheet = workbook.add_worksheet('q4_' + sn)
+#     # export_excel(worksheet, all_list[3])
 
-
-#     # now that we have the raw values, let's calculate the weighted scores.
-#     q4 = []
-#     for r1 in all_list[0]:
-#         for r2 in all_list[1]:
-#             if (r1[0] == r2[0]) and (r1[0] != "uuid"):
-#                 new_val_row = r1[:1] + list(map(lambda x: get_weight_score(x), zip(r1[1:], r2[1:])))
-#                 q4.append(new_val_row)
-#     all_list.append(q4)            
-
-#     worksheet = workbook.add_worksheet('q1_' + sn)
-#     export_excel(worksheet, all_list[0])
-#     worksheet = workbook.add_worksheet('q2_' + sn)
-#     export_excel(worksheet, all_list[1])
-#     worksheet = workbook.add_worksheet('q3_' + sn)
-#     export_excel(worksheet, all_list[2])
-#     worksheet = workbook.add_worksheet('q4_' + sn)
-#     export_excel(worksheet, all_list[3])
-
-# workbook.close() # done writing
+workbook.close() # done writing
